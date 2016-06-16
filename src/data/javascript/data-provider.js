e928b28d386fafa8af74b42b91282ff4c9f52459
felipe07 <felipe07@gmail.com>
2016-06-16 17:47:51 +0200

    First prototype of customizable charts. Not working yet

---
+++ src/data/javascript/data-provider.js
@@ -0,0 +1,38 @@
+var from = "2015-01-01T00:00:00+0200";
+var until = "2015-06-30T00:00:00+0200";
+
+function getData(from, until) {
+
+  var dataURL = "http://131.175.143.71/istsos/test?" +
+                "service=SOS&version=1.0.0&" +
+                "request=GetObservation&" + 
+                "offering=offering1&" +
+                "procedure=MI_Lambrate_Temperatura&" + 
+                //"procedure=MI_Lambrate_Precipitazione&" +
+                "eventTime=" + from + "/" + until + "&" +
+                "observedProperty=temperature&" + 
+                //"observedProperty=rainfall&" + 
+                //"aggregateInterval=PT24H&" + 
+                //"aggregateFunction=SUM&" +
+                //"qualityIndex=True&" +
+                //"qualityFilter=>110&" +
+                "responseFormat=application/json";
+ 
+  var measurements = [];
+
+  $.getJSON(dataURL, function(data) {
+    // var data contains values, fields and elementCount
+    var data = jQuery.extend({}, data["ObservationCollection"].member[0].result.DataArray);
+    var values = data["values"].slice();
+    var measurements = [];
+      
+    for (var i = 0; i < values.length; i++) {
+      var measurement = {
+        "date": values[i][0],
+        "measurement": values[i][1]
+      }
+      measurements.push(measurement);
+    }
+  })
+  .success(function() { return measurements; });
+}
